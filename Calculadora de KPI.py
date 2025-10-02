b3cc5318804af89def07bfcdae6ca3c810c9449c
vararatc-cpu <vararatc@unemi.edu.ec>
2025-10-02 15:16:30 -0500

    asrg

--- Calculadora de KPI.py
+++ Calculadora de KPI.py
@@ -2,22 +2,14 @@ import tkinter as tk
 from tkinter import filedialog
 from PIL import Image, ImageTk
 
-def abrir_imagen():
-    ruta = filedialog.askopenfilename(filetypes=[("Imágenes", "*.png;*.jpg;*.jpeg")])
-    if ruta:
-        img = Image.open(ruta)
-        img = img.resize((300, 300))  # ajustar tamaño
-        foto = ImageTk.PhotoImage(img)
-        etiqueta_imagen.config(image=foto)
-        etiqueta_imagen.image = foto
 
 ventana = tk.Tk()
 ventana.title("Visor simple de imágenes")
+ventana.geometry("400x300")  # ancho x alto en píxeles
+ventana.resizable(False, False)  # no se puede agrandar ni achicar
+ventana.configure(bg="lightblue")
 
-etiqueta_imagen = tk.Label(ventana, text="No hay imagen")
-etiqueta_imagen.pack(padx=10, pady=10)
-
-boton = tk.Button(ventana, text="Abrir imagen", command=abrir_imagen)
-boton.pack(pady=5)
+etiqueta = tk.Label(ventana, text="Hola, soy un texto en la ventana", font=("Arial", 12), fg="blue")
+etiqueta.pack(pady=20)  # .pack coloca el widget dentro de la ventana
 
 ventana.mainloop()
